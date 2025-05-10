1e2d7b4f080997189d5eaa788e9b8f555196b403
TuiLaZit <147925219+TuiLaZit@users.noreply.github.com>
2025-05-10 07:06:32 +0700

    Update menu.php
    chỉnh lại cơ cấu menu

--- admincp/modules/menu.php
+++ admincp/modules/menu.php
@@ -1,27 +1,17 @@
 <div class="menu">
     <ul class="admin-menu">
         <li class="dropdown">
-            <a href="#" class="dropdown-toggle">Quản lý nhân viên ▼</a>
-            <ul class="dropdown-menu">
-                <li><a href="index.php?action=staff">Nhân viên</a></li>
-                <li><a href="index.php?action=delivery">Đơn vị vận chuyển</a></li>
-            </ul>
+            <a href="#" class="dropdown-toggle">Quản lý nhân viên</a>
+
         </li>
         <li class="dropdown">
-            <a href="#" class="dropdown-toggle">Quản lý khách hàng ▼</a>
-            <ul class="dropdown-menu">
-                <li><a href="index.php?action=customers">Khách hàng</a></li>
-                <li><a href="index.php?action=customer_care">Chăm sóc khách hàng</a></li>
-            </ul>
+            <a href="#" class="dropdown-toggle">Quản lý khách hàng</a>
         </li>
         <li class="dropdown">
             <a href="#" class="dropdown-toggle">Quản lý sản phẩm ▼</a>
             <ul class="dropdown-menu">
                 <li><a href="index.php?action=products">Sản phẩm</a></li>
                 <li><a href="index.php?action=groups">Nhóm</a></li>
-                <li><a href="index.php?action=members">Thành viên</a></li>
-                <li><a href="index.php?action=versions">Version</a></li>
-                <li><a href="index.php?action=promotions">Khuyến mãi</a></li>
                 <li><a href="index.php?action=imports">Nhập hàng</a></li>
             </ul>
         </li>
