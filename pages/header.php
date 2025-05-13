fbe5885b556eda911150ded34e61681a663c9320
TuiLaZit <147925219+TuiLaZit@users.noreply.github.com>
2025-05-13 22:21:58 +0700

    Update header.php

--- pages/header.php
+++ pages/header.php
@@ -7,19 +7,19 @@
     <div class="right-tab">
         <ul class="rbuttontab">
             <li>
-                <a href="./index.php?quanly=giohang" class="rightbutton">Giỏ Hàng</a>
+                <a href="/index.php?quanly=giohang" class="rightbutton">Giỏ Hàng</a>
             </li>
             <li>
-                <a href="index.php?quanly=thongbao" class="rightbutton">Thông Báo</a>
+                <a href="/index.php?quanly=thongbao" class="rightbutton">Thông Báo</a>
             </li>
 
             <?php if (isset($_SESSION["user"])): ?>
                 <li>
-                    <a href="./controller/login-customer.php?logout" class="rightbutton">Đăng Xuất</a>
+                    <a href="/./controller/login-customer.php?logout" class="rightbutton">Đăng Xuất</a>
                 </li>
             <?php else : ?>
                 <li>
-                    <a href="./login.php" class="rightbutton">Đăng Nhập</a>
+                    <a href="/login.php" class="rightbutton">Đăng Nhập</a>
                 </li>
             <?php endif  ?>
 
