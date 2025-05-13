bb44265b9bfd37feaae40acbb28194291dc27f65
pt287 <151818767+pt287@users.noreply.github.com>
2025-05-13 18:12:58 +0700

    thêm chuyển sang giỏ hàng

--- pages/main/main.php
+++ pages/main/main.php
@@ -1,11 +1,26 @@
 <div id="main" class="main">
     <?php
-    include("banner.php");
+    if (isset($_GET['quanly'])) {
+        switch ($_GET['quanly']) {
+            case 'giohang':
+                include __DIR__ . '/../cart.php';
+                break;
+            default:
+                include("banner.php");
+                echo '<div id="main-page">';
+                include("searchbar.php");
+                echo '<div id="main-container">';
+                include("main_page_container.php");
+                echo '</div></div>';
+                break;
+        }
+    } else {
+        include("banner.php");
+        echo '<div id="main-page">';
+        include("searchbar.php");
+        echo '<div id="main-container">';
+        include("main_page_container.php");
+        echo '</div></div>';
+    }
     ?>
-    <div id="main-page">
-        <?php include("searchbar.php"); ?>
-        <div id="main-container">
-            <?php include("main_page_container.php"); ?>
-        </div>
-    </div>
 </div>
