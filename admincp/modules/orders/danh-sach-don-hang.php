ed7eb0287d2e156c3dc42642382739c97d2ae757
pt287 <151818767+pt287@users.noreply.github.com>
2025-05-13 21:30:37 +0700

    Chỉnh lại đường dẫn

--- admincp/modules/orders/danh-sach-don-hang.php
+++ admincp/modules/orders/danh-sach-don-hang.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'C:/xampp/htdocs/web2-main/admincp/config/config.php';
+require_once(__DIR__ . '/../../config/config.php');
 
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
