fd3da7d00a7f097638510de1d976fb6cb64e4353
TuiLaZit <147925219+TuiLaZit@users.noreply.github.com>
2025-05-15 20:38:16 +0700

    update thống kê

--- admincp/modules/statistics.php
+++ admincp/modules/statistics.php
@@ -2,9 +2,17 @@
 require_once(__DIR__. '/../config/config.php');
 ?>
 <div>
+    <h1>Thống kê hóa đơn</h1>
     <div id="search-stat">
         <input type="date" id="dateFrom">
         <input type="date" id="dateTo">
+
+        <select id="sortOrder">
+        <option value="ASC">Tăng dần</option>
+        <option value="DESC">Giảm dần</option>
+    </select>
+
+
         <button id="searchStat">Tìm Kiếm</button>
     </div>
     <div id="stat">
