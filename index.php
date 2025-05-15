439b8538180628a03921599f963f71f2938ed4a1
TuiLaZit <147925219+TuiLaZit@users.noreply.github.com>
2025-05-15 08:50:21 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" type="text/css" href="./css/main.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <title>NJZ gShop</title>
+    <title>KCorner</title>
 </head>
 
 <body>
