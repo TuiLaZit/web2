1217ba3f2f629a087deed56955ad99f1b9d921ea
TuiLaZit <147925219+TuiLaZit@users.noreply.github.com>
2025-05-16 15:52:18 +0700

    Add files via upload

--- admincp/modules/statistics.php
+++ admincp/modules/statistics.php
@@ -6,7 +6,7 @@ require_once(__DIR__. '/../config/config.php');
   <div style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;">
     <div
             class="bg-color"
-            style="height: 100vh; width: 100%; overflow-y: auto; position: relative;">
+            style="height: 100vh;padding-left:1em; width: 100%; overflow-y: auto; position: relative;">
             <h1>Thống kê hóa đơn</h1>
             <div id="search-stat">
                 <input type="date" id="dateFrom">
