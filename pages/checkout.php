efa242294be2e8a14038c4083de714fd01874ef6
pt287 <151818767+pt287@users.noreply.github.com>
2025-05-16 22:55:01 +0700

    thêm nút

--- pages/checkout.php
+++ pages/checkout.php
@@ -161,8 +161,9 @@ $total = 0;
 <link rel="stylesheet" type="text/css" href="../css/style.css">
 <link rel="stylesheet" type="text/css" href="../css/cart.css">
 <script src="../admincp/js/vietnamese-provinces-data.js"></script> 
-<div class="wrapper">
+<div class="cart_wrapper">
     <div class="cart-container" style="max-width:700px;">
+        <a href="javascript:history.back()" class="back-btn">← Quay lại</a>
         <h1>Xác nhận đơn hàng</h1>
         <?php if ($order_success): ?>
             <div class="checkout-section" style="max-width:600px;margin:40px auto;">
